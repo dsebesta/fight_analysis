2d1978d2da7f43d4be4426dddc070d05e843bad4
Derrick Sebesta <dsebesta@gmail.com>
2017-05-25 21:55:53 -0700

    almost done with fighter detail scrape. need to fix for fighters who dont have birthday listed

--- src/server/util/scrape_fighter_info.js
+++ src/server/util/scrape_fighter_info.js
@@ -7,13 +7,82 @@ module.exports.getFighterData = (url) => {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
                 const fighter_stats = {
+                    wins: null,
+                    wins_ko: null,
+                    wins_sub: null,
+                    wins_dec: null,
+                    wins_other: null,
+                    losses: null,
+                    losses_ko: null,
+                    losses_sub: null,
+                    losses_dec: null,
+                    losses_other: null,
+                    no_contest: null,
                     height: null,
-                    weight_class: null
+                    weight: null,
+                    weight_class: null,
+                    age: null,
+                    birthday: null,
+                    locality: null,
+                    nationality: null,
+                    association: null,
+
                 };
-                const tempHeight = $('.height').text();
-                const regex = /[0-9.]{6}/g;
-                fighter_stats.height = tempHeight.match(regex)[0];
-                fighter_stats.weight_class = $('.wclass').find('a').text();
+
+
+                $('.record .count_history').filter(function() {
+                    const el = $(this);
+                    const wins = el.find('.left_side .bio_graph').first();
+                    const winsByKnockout = wins.find('.graph_tag:nth-child(3)');
+                    const winsBySubmission = wins.find('.graph_tag:nth-child(5)');
+                    const winsByDecision = wins.find('.graph_tag:nth-child(7)');
+                    const winsByOther = wins.find('.graph_tag:nth-child(9)');
+                    const losses = el.find('.left_side .bio_graph.loser');
+                    const lossesByKnockout = losses.find('.graph_tag:nth-child(3)');
+                    const lossesBySubmission = losses.find('.graph_tag:nth-child(5)');
+                    const lossesByDecision = losses.find('.graph_tag:nth-child(7)');
+                    const lossesByOther = losses.find('.graph_tag:nth-child(9)');
+                    const noContests = el.find('.right_side .bio_graph');
+                    const getTotal = function(el) { return parseInt(el.text().split(' ')[0] || 0); };
+
+                    const wins_total = parseInt(wins.find('.card .counter').text());
+                    const losses_total = parseInt(losses.find('.counter').text());
+                    const no_contests_total = noContests.find('.counter');
+                    fighter_stats.wins = wins_total;
+                    fighter_stats.losses = losses_total;
+                    fighter_stats.no_contest = getTotal(no_contests_total);
+                    fighter_stats.wins_ko = getTotal(winsByKnockout);
+                    fighter_stats.wins_sub = getTotal(winsBySubmission);
+                    fighter_stats.wins_dec = getTotal(winsByDecision);
+                    fighter_stats.wins_other = getTotal(winsByOther);
+                    fighter_stats.losses_ko = getTotal(lossesByKnockout);
+                    fighter_stats.losses_sub = getTotal(lossesBySubmission);
+                    fighter_stats.losses_dec = getTotal(lossesByDecision);
+                    fighter_stats.losses_other = getTotal(lossesByOther);
+                });
+
+                $('.bio').filter(function() {
+                    var el = $(this);
+                    const age = el.find('.item.birthday strong').text();
+                    const birthday = el.find('span[itemprop="birthDate"]').text();
+                    const locality = el.find('span[itemprop="addressLocality"]').text();
+                    const nationality = el.find('strong[itemprop="nationality"]').text();
+                    const association = el.find('.item.association span[itemprop="name"]').text();
+                    const height = el.find('.item.height strong').text();
+                    const weight = el.find('.item.weight strong').text();
+                    const weight_class = el.find('.item.wclass strong').text();
+                    const regex = /[0-9.]{5,}/g;
+
+                    fighter_stats.age = parseInt(age.slice(5));
+                    fighter_stats.birthday = birthday;
+                    fighter_stats.locality = locality;
+                    fighter_stats.nationality = nationality;
+                    fighter_stats.association = association;
+                    fighter_stats.height = height;
+                    fighter_stats.weight = weight;
+                    fighter_stats.weight_class = weight_class;
+                });
+
 
                 resolve(fighter_stats);
             }
--- src/server/util/test.js
+++ src/server/util/test.js
@@ -1,7 +1,8 @@
 var scrape_fighter = require('./scrape_fighter_info');
 
 
-const url = 'http://www.sherdog.com/fighter/Alexander-Gustafsson-26162';
+// const url = 'http://www.sherdog.com/fighter/Alexander-Gustafsson-26162';
+const url = 'http://www.sherdog.com/fighter/Eddie-Alvarez-9265';
 let pullData = scrape_fighter.getFighterData(url);
 pullData.then((data) => {
     console.log(data)
