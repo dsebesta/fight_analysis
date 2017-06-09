b1f2e2cfa348de5c6d3dc9079f600c1534abdd87
Derrick Sebesta <dsebesta@gmail.com>
2017-06-09 13:29:27 -0700

    more formatting

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,19 +10,16 @@ import Event from './Event';
 
 const Nav = () => {
     return (
-        <div className="nav-bar">
-            <Toolbar>
-                <ToolbarGroup>
-                    <Link to='/fight'>
-                        Fight Analyzer
-                    </Link>
-                </ToolbarGroup>
-            </Toolbar>
+        <div>
+            <div className="nav-bar">
+                <i className="material-icons">menu</i>
+                <h3><Link to="/fight">Fight Analyzer</Link></h3>
+            </div>
+
             <Switch >
                 <Route exact path="/events/:id" component={Event} />
                 <Route exact path="/events/:id/:match" component={Matchup} />
             </Switch>
-
         </div>
 
     )
